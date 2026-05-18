4e8785a061914f8f7d8ed8706b60dbe9c9df65e7
akchoudhary1114773136aA <131717999+akchoudhary1114773136aA@users.noreply.github.com>
2026-05-18 13:15:45 +0530

    Update frontend text color theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
 
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
